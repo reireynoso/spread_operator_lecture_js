4bbc2ff02881db53089551a46accc6d54d29fca4
reireynoso <reireynoso@gmail.com>
2020-01-23 12:05:56 -0500

    lecture

--- main.js
+++ main.js
@@ -5,6 +5,45 @@
 // 3. In react, we do not mutate state. State in memory triggers rerender. spread operator changes object in memory
 // 4. setState uses spread under hood.
 
+// const rei2018 = {
+//     name: "Rei",
+//     employer: "Board of Education",
+//     city: "Jersey City",
+//     tired: true,
+//     info: {
+//         hair: "black",
+//         wearsGlasses: false
+//     },
+//     programmingLanguages: []
+// }
+
+// Pass By Value
+let testingThisOut = "test"
+
+let test = testingThisOut
+
+// console.log(test)
+
+testingThisOut = "Eric"
+// test = testingThisOut
+
+// console.log(testingThisOut)
+// console.log(test)
+
+// Pass By Reference
+
+
+
+// Both 2019 and 2018 are pointing to the same space in memory
+
+// const rei2019 = rei2018 
+// const rei2019 = Object.assign({}, rei2018)
+// const rei2019 = {
+//     ...rei2018
+// }
+
+const learnedLanguages = []
+
 const rei2018 = {
     name: "Rei",
     employer: "Board of Education",
@@ -14,22 +53,39 @@ const rei2018 = {
         hair: "black",
         wearsGlasses: false
     },
-    programmingLanguages: []
+    programmingLanguages: learnedLanguages
 }
 
-// const rei2019
+
+const changes = {
+    employer: "Access Labs",
+    city: "Brooklyn",
+    programmingLanguages: learnedLanguages,
+    stillTired: true
+}
+
+const rei2019 = {
+    ...rei2018,
+    ...changes,
+    // employer: "Access Labs",
+    // city: "Brooklyn",
+    // programmingLanguages: [],
+    // stillTired: true,
+    // tired: true,
+    // info: {
+    //     hair: "black",
+    //     wearsGlasses: false
+    // },
+}
+
+// rei2019.programmingLanguages.push("Ruby")
+// rei2019.programmingLanguages.push("Javascript")
 
 
-// console.log(rei2018)
 
+// console.log("rei2018", rei2018)
+// console.log("rei2019", rei2019)
 
-
-
-// const changes = {
-//     employer: "Access Labs",
-//     city: "Brooklyn",
-//     programmingLanguages: ["JS", "Ruby"]
-// }
 
 // const rei2019 = {
 //     ...rei2018,
@@ -44,41 +100,30 @@ const rei2018 = {
 
 // Could I do it to an array?
   
+
   
-const firstThreeMonths = ["January", "February", "March"]; 
 
-const winterMonths = firstThreeMonths
 
-winterMonths.push("December")
+// const winterMonths = firstThreeMonths
+
+// winterMonths.push("December")
 
 // console.log(winterMonths)
+// console.log(firstThreeMonths)
 
-
-
-
-
-
-
-
-
-
+const firstThreeMonths = ["January", "February", "March"]; 
 const lastSixMonths = ["July", "August", "September", "October", "November", "December"];
 
 
-const firstSixMonths = [firstThreeMonths[0], firstThreeMonths[1], firstThreeMonths[2], "April", "May", "June"]
+const firstSixMonths = [...firstThreeMonths, "April", "May", "June"]
 
-const firstHalfOfTheYear = []
+const firstHalfOfTheYear = [...firstSixMonths]
 
+// firstHalfOfTheYear.push("Mazen")
 
-
-
-
-
-
-
-
-
-
+// console.log(firstSixMonths === firstHalfOfTheYear)
+// console.log(firstSixMonths)
+// console.log(firstHalfOfTheYear)
 
 
 //   const firstSixMonths = [...firstThreeMonths, "April", "May", "June"]
@@ -93,7 +138,45 @@ const firstHalfOfTheYear = []
 
   
 
+
 // Could I do it to the parameters of a function?
+
+
+function giveMeSomeParams(arg1, arg2, arg3){
+    // let arg1;
+    console.log(arg1)
+    // console.log(arg2)
+    // console.log(arg3)
+}
+
+const mazen = ["Chocolate", "Dogs", "Eric", "Alex"]
+
+giveMeSomeParams(...mazen)
+// giveMeSomeParams("Chocolate", "Dogs", "Eric", "Alex")
+// ...mazen => "Chocolate", "Dogs", "Eric"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
   // function canTakeSixArguments(arg1, arg2, arg3) {
